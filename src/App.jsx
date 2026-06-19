1339dd6b026e5b8d6bad379203189b6665070299
pankajcorextrime <pankajcorextrime@gmail.com>
2026-06-19 15:43:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch("http://localhost:7777/user", {
+          credentials: "include",
+        });
+
+        console.log(res);
+        const data = await res.json();
+        console.log(data);
+        if (res.status === 401) {
+          navigate("/login");
+          return;
+        }
+        if (!res.ok) {
+          console.log(data.message);
+          return;
+        }
+
+        setUsers(data.user || []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to Home page</h1>
+
+      {users.length === 0 ? (
+        <p>No users found</p>
+      ) : (
+        users.map((user) => (
+          <div key={user._id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Home;
